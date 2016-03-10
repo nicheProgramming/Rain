c1630ed1fbc316e0636bf088b74f77d4108d6dc4
nicheProgramming <captainless2@gmail.com>
2016-03-10 17:54:20 -0600

    Got to the link in the description
    https://youtu.be/2SUtcxJYN6k?list=PLlrATfBNZ98eOOCk2fOFg7Qg5yoQfFAdf

--- src/com/omega/rain/Game.java
+++ src/com/omega/rain/Game.java
@@ -20,18 +20,18 @@ public class Game extends Canvas implements Runnable {
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
-	
+
 	private Screen screen;
 
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		
+
 		screen = new Screen(width, height);
-		
+
 		frame = new JFrame();
 	}
 
@@ -51,10 +51,19 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void run() {
+		long lastTime = System.nanoTime();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
 		while (running) {
-			update();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			while (delta >= 1) {
+				update();
+				delta--;
+			}
 			render();
 		}
+		stop();
 	}
 
 	public void update() {
@@ -67,10 +76,10 @@ public class Game extends Canvas implements Runnable {
 			createBufferStrategy(3);
 			return;
 		}
-		
+
 		screen.clear();
 		screen.render();
-		
+
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
 		}
@@ -79,7 +88,8 @@ public class Game extends Canvas implements Runnable {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
-		g.dispose();;
+		g.dispose();
+		;
 		bs.show();
 	}
 
