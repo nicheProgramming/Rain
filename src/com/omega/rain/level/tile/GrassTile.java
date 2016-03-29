a7bd1af18030f765d3fb59fcbba1d3a47ad8e90d
nicheProgramming <captainless2@gmail.com>
2016-03-29 18:46:14 -0500

    Finished Episode 27 in hopes it may magically fix the error
    it didn't.

---
+++ src/com/omega/rain/level/tile/GrassTile.java
@@ -0,0 +1,16 @@
+package com.omega.rain.level.tile;
+
+import com.omega.rain.graphics.Screen;
+import com.omega.rain.graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		//Do render stuff here!
+	}
+	
+}
