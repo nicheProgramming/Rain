be71592e44978ac24876d334316a3db102aea655
nicheProgramming <captainless2@gmail.com>
2016-04-16 19:08:20 -0500

    Finished Episode 31

--- src/com/omega/rain/graphics/Screen.java
+++ src/com/omega/rain/graphics/Screen.java
@@ -5,7 +5,7 @@ import com.omega.rain.level.tile.Tile;
 
 public class Screen {
 
-	private int width, height;
+	public int width, height;
 	public int[] pixels;
 	public final int MAP_SIZE = 64;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
@@ -44,6 +44,11 @@ public class Screen {
 	public void renderTile(int xp, int yp, Tile tile) {
 		for(int y = 0; y < tile.sprite.SIZE; y++) {
 			int ya = y + yp;
+			for (int x = 0; x < tile.sprite.SIZE; y++) {
+				int xa = x + xp;
+				if (xa < 0 || xa  >= width || ya < 0 || ya >= width) break;
+				pixels[xa + ya * width] = tile.sprite.pixels[x + y * tile.sprite.SIZE];
+			}
 		}
 	}
 	
